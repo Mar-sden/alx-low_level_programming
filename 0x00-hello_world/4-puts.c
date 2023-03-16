7072fe01473930bd0d921c5d2513741ae52f7587
root <huncho8956@gmail.com>
2023-03-16 04:24:10 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
- * main - this is the main function
- *
- * Return: always 0
- */
+*main - entry poit
+*return : always 0 (success)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle):
-	return (0):
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
