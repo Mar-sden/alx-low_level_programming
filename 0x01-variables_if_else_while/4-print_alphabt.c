dbab50007e9fd384aab0e3c95e6b17d5f162bdf0
root <huncho8956@gmail.com>
2023-03-17 05:39:06 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints lowercase alphabets
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 'a'; y <= 'z'; y++)
+	{
+		if (y != 'e' && y != 'q')
+		{
+			putchar(y);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
